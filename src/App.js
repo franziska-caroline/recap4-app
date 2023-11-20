31683e561c58795dfe435a6fa417a3b947206176
Franziska Adam <franziskacarolineadam@gmail.com>
2023-11-20 11:46:21 +0100

    Update Form

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import "./App.css";
 import Form from "./components/Form";
+import { useState } from "react";
+import { uid } from "uid";
 
 function App() {
+  const [activity, setActivity] = useState("");
+
+  function handleAddActivity(newActivity) {
+    // adds a new animal to the state + passed down to `Form`
+    setActivity([...activity, { id: uid(), ...newActivity }]);
+  }
+
   return (
     <div className="App">
-      <Form />
+      <Form onAddActivity={handleAddActivity} />
     </div>
   );
 }

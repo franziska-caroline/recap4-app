f6d7ec8e19a4ce91cf686d07050a9aabb3993020
Franziska Adam <franziskacarolineadam@gmail.com>
2023-11-20 16:02:56 +0100

    Add Local Storage

--- src/components/List.js
+++ src/components/List.js
@@ -1,25 +1,14 @@
-import Form from "./Form"
+import Form from "./Form";
 
 export default function List({ activities }) {
-    console.log(activities)
-    return (
-        
-            <ul className="activity-list">
-                {activities.map((activity) => (
-                    
-                    <li key={activity.id} className="activity-list__item">
-                        {activity.name}
-                    </li>
-                   
-))}
-            
-            </ul>
-
-
-
-
-
-    )
-
-   
+  console.log(activities);
+  return (
+    <ul className="activity-list">
+      {activities.map((activity) => (
+        <li key={activity.id} className="activity-list__item">
+          {activity.name}
+        </li>
+      ))}
+    </ul>
+  );
 }

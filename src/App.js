256b1f21a2293683786fd10898767fc4e5f8e8db
Marta Jäkel <martajaekel@gmail.com>
2023-11-21 09:48:37 +0100

    add

--- src/App.js
+++ src/App.js
@@ -1,27 +1,23 @@
-import List from "./components/List";
+import List  from "./components/List";
 import "./App.css";
 import Form from "./components/Form";
 import { useState } from "react";
 import { uid } from "uid";
 import useLocalStorageState from "use-local-storage-state";
-
 function App() {
   const [activities, setActivities] = useLocalStorageState("activities", {
     defaultValue: [],
   });
-
   const isGoodWeather = true;
-
   const filterList = activities.filter((activity) => {
     return activity.isForGoodWeather === isGoodWeather;
   });
-
   function handleAddActivity(newActivity) {
     // adds a new animal to the state + passed down to `Form`
     setActivities([...activities, { id: uid(), ...newActivity }]);
-    console.log(newActivity);
+   
   }
-  console.log(activities);
+
   return (
     <div className="App">
       <h1 className="form__header">Weather App</h1>
